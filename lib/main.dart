23a2b412d89c2582e395b94d2c5492f0cf0495be
alsayeed <alxayeed@gmail.com>
2023-07-01 16:17:02 +0600

    Add: simple UI added

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
+
+  String _batteryLevel = "0";
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Battery Level: $_batteryLevel'),
+                const SizedBox(height: 12.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Get Battery Level"),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
